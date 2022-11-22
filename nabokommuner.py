c0198bf642d3151d10aa03b6024fef12e8003867
mortewle <70330502+mortewle@users.noreply.github.com>
2022-11-22 19:21:41 +0100

    Update nabokommuner.py

--- nabokommuner.py
+++ nabokommuner.py
@@ -9,14 +9,13 @@ def les_geoparquet(sti):
     return gpd.read_file(sti)
 
 
-# lager ordbok (dict) der f.eks. nabodict["0301"] gir en liste over Oslos nabokommuner
-# hvis kommune er spesifisert, returneres liste med nabokommuner
+
 def nabokommuner(kommune=None, 
                  aar=None):
     
     """
-    nabokommuner("0301", 2017) -> ['0213', '0229', '0217', '0219', '0605', '0230', '0231', '0233', '0533']
-    nabokommuner() -> ordbok der kommunenumrene er keys og nabokommunene values (kommuner fra året vi er i)
+    Lager ordbok (dict) der f.eks. nabodict["0301"] gir en liste over Oslos nabokommuner
+    Hvis kommune er spesifisert, returneres liste med nabokommuner    nabokommuner("0301", 2017) -> ['0213', '0229', '0217', '0219', '0605', '0230', '0231', '0233', '0533']
     """
     
     if aar is None:
@@ -27,7 +26,7 @@ def nabokommuner(kommune=None,
                          layer="kommune")
     abas["KOMMUNENR"] = abas.kommunenummer
 
-    if kommune is not None:
+    if kommune:
         
         #spatial join mellom kommunen og alle andre andre kommuner
         kommunen = abas.loc[abas.KOMMUNENR == kommune, ["geometry"]]
@@ -45,7 +44,7 @@ def nabokommuner(kommune=None,
         andre_kommuner = abas.loc[abas.KOMMUNENR != kommune, ["KOMMUNENR", "geometry"]]
         joinet = kommunen.sjoin(andre_kommuner)
 
-        # gjør liste over unike kommunenumre til value og kommunen til key
+        # gjør liste over kommunenumre til value og kommunen til key
         naboer = list(joinet.KOMMUNENR.unique())
         nabodict[kommune] = naboer
 
